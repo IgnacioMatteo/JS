256d266d2165bb5b5bb9f3dbeb8c44b1757ba600
Ignacio Matteo <ignacio.matteo27@gmail.com>
2020-11-29 11:50:48 -0300

    Creacion de la funsion

---
+++ script.js
@@ -0,0 +1,9 @@
+let edad = prompt("¡Hola!, indicanos tu edad");
+
+if (edad < 18) {
+    console.log("No se te puede vender alcohol, sos menor de edad")
+} else if (edad > 18){
+    console.log("Bienvenido, ¿que bebida alcoholica queres?")
+} else {
+    console.log("Bienvenido, ¿que bebida alcoholica queres?")
+}
