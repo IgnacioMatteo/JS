4810d06ed9dfa137bafb0ca081f6a8e299e77a9f
Ignacio Matteo <ignacio.matteo27@gmail.com>
2020-12-10 19:50:59 -0300

    se modifica el index

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// >= 18 se le vende alcohol
-let edad = prompt("¡Hola!, indicanos tu edad");
+// >= Preguntamos que seguto quiere cotizar
+let pregunta = prompt("¡Hola! ¿te gustaria cotizar un seguro de Celular?");
 
-if (edad < 18) {
-    console.log("No se te puede vender alcohol, sos menor de edad")
-} else if (edad > 18){
-    console.log("Bienvenido, ¿que bebida alcoholica queres?")
-} else {
+if (pregunta = si) {
+    console.log("Tenemos un seguro de $10.000 que sale $800 mensuales")
+} else if (edad = no){
     console.log("Bienvenido, ¿que bebida alcoholica queres?")
 }
