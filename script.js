505cd2b8c7194fd9968f8a710da197ab2ed22fea
Ignacio Matteo <ignacio.matteo27@gmail.com>
2020-12-10 19:57:19 -0300

    se modifica el script

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // >= Preguntamos que seguto quiere cotizar
 let pregunta = prompt("¡Hola! ¿te gustaria cotizar un seguro de Celular?");
 
-if (pregunta = si) {
-    console.log("Tenemos un seguro de $10.000 que sale $800 mensuales")
-} else if (edad = no){
-    console.log("Bienvenido, ¿que bebida alcoholica queres?")
+if (pregunta === si) {
+    console.log("Tenemos un seguro de $10.000 que sale $800 mensuales ¿te interesa?")
+} else if (pregunta = si){
+    console.log("Perfecto, ya tienes tu seguro!")
 }
