9c13a135fbfd126f9d9ca7fbbd31fd42ac5d2ece
Ignacio Matteo <ignacio.matteo27@gmail.com>
2020-12-17 20:57:53 -0300

    creacion de objeto parte1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // >= Preguntamos que seguto quiere cotizar
 let pregunta = prompt("¡Hola! ¿te gustaria cotizar un seguro de Celular?");
 
-if (pregunta === si) {
+if (pregunta = si) {
     console.log("Tenemos un seguro de $10.000 que sale $800 mensuales ¿te interesa?")
 } else if (pregunta = si){
     console.log("Perfecto, ya tienes tu seguro!")
